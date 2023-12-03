c51a9e00feae7492238d329a8c4b3d6cf14ef8cf
Sam <144938568+SamLo2200@users.noreply.github.com>
2023-12-03 23:28:59 +0800

    first commit

---
+++ hangman.css
@@ -0,0 +1,85 @@
+body {
+  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue",
+    Helvetica, Arial, "Lucida Grande", sans-serif;
+  background-color: #c1d72e;
+  color: white;
+  text-align: center;
+  height: 100%;
+  font-size: 18px;
+}
+
+.wrappper {
+  width: 100%;
+  margin: 0 auto;
+}
+
+canvas {
+  color: white;
+  border: white dashed 2px;
+  padding: 15px;
+}
+
+h1,
+h2,
+h3 {
+  font-family: "Roboto", sans-serif;
+  font-weight: 100;
+  text-transform: uppercase;
+  margin: 5px 0;
+}
+h1 {
+  font-size: 2.6em;
+}
+
+h2 {
+  font-size: 1.2em;
+}
+
+p {
+  font-size: 1.2em;
+}
+
+Button {
+  background: #c1d72e;
+  border-radius: 5px;
+  border: solid 1px white;
+  color: white;
+  width: 35px;
+  height: 35px;
+  margin: 0 10px 10px 0;
+  outline: none;
+}
+Button:hover {
+  transition: 0.5s;
+  background: white;
+  border: solid 1px white;
+  color: #c1d72e;
+}
+
+.selected {
+  background: white;
+  border: solid 1px white;
+  color: #c1d72e;
+}
+
+.btn-2 {
+  border-radius: 10px;
+  background: #c1d72e;
+  color: white;
+  border: solid 1px white;
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 1.2em;
+  padding: 10px 10px;
+  width: 180px;
+  height: 50px;
+  margin: 0 10px 10px 0;
+
+  outline: none;
+}
+.btn-2:hover {
+  transition: 0.5s;
+  background: white;
+  border: solid 1px white;
+  color: #c1d72e;
+}
---
+++ hangman.js
@@ -0,0 +1,275 @@
+//const
+const container = document.getElementById("alphabetButtons");
+var answerDisplay = document.getElementById("hold");
+var answer = "";
+var hint = "";
+var life = 10;
+var wordDisplay = [];
+var winningCheck = "";
+const containerHint = document.getElementById("clue");
+const buttonHint = document.getElementById("hint");
+const buttonReset = document.getElementById("reset");
+const livesDisplay = document.getElementById("mylives");
+var myStickman = document.getElementById("stickman");
+var context = myStickman.getContext("2d");
+
+//generate alphabet button
+function generateButton() {
+  var buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+        `<button
+         class = "alphabetButtonJS" 
+         id="${letter}"
+         >
+        ${letter}
+        </button>`
+    )
+    .join("");
+
+  return buttonsHTML;
+}
+
+function handleClick(event) {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (isButton) {
+    //console.dir(event.target.id);
+    //console.log(isButton);
+    const buttonId = document.getElementById(event.target.id);
+    buttonId.classList.add("selected");
+  }
+  return;
+}
+
+//word array
+const question = [
+  "The Chosen Category Is Premier League Football Teams",
+  "The Chosen Category Is Films",
+  "The Chosen Category Is Cities"
+];
+
+const categories = [
+  [
+    "everton",
+    "liverpool",
+    "swansea",
+    "chelsea",
+    "hull",
+    "manchester-city",
+    "newcastle-united"
+  ],
+  ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
+  ["manchester", "milan", "madrid", "amsterdam", "prague"]
+];
+
+const hints = [
+  [
+    "Based in Mersyside",
+    "Based in Mersyside",
+    "First Welsh team to reach the Premier Leauge",
+    "Owned by A russian Billionaire",
+    "Once managed by Phil Brown",
+    "2013 FA Cup runners up",
+    "Gazza's first club"
+  ],
+  [
+    "Science-Fiction horror film",
+    "1971 American action film",
+    "Historical drama",
+    "Anamated Fish",
+    "Giant great white shark"
+  ],
+  [
+    "Northern city in the UK",
+    "Home of AC and Inter",
+    "Spanish capital",
+    "Netherlands capital",
+    "Czech Republic capital"
+  ]
+];
+
+//set question,answer and hint
+
+function setAnswer() {
+  const categoryOrder = Math.floor(Math.random() * categories.length);
+  const chosenCategory = categories[categoryOrder];
+  const wordOrder = Math.floor(Math.random() * chosenCategory.length);
+  const chosenWord = chosenCategory[wordOrder];
+
+  const categoryNameJS = document.getElementById("categoryName");
+  categoryNameJS.innerHTML = question[categoryOrder];
+
+  //console.log(chosenCategory);
+  //console.log(chosenWord);
+  answer = chosenWord;
+  hint = hints[categoryOrder][wordOrder];
+  answerDisplay.innerHTML = generateAnswerDisplay(chosenWord);
+}
+
+function generateAnswerDisplay(word) {
+  var wordArray = word.split("");
+  //console.log(wordArray);
+  for (var i = 0; i < answer.length; i++) {
+    if (wordArray[i] !== "-") {
+      wordDisplay.push("_");
+    } else {
+      wordDisplay.push("-");
+    }
+  }
+  return wordDisplay.join(" ");
+}
+
+function showHint() {
+  containerHint.innerHTML = `Clue - ${hint}`;
+}
+
+buttonHint.addEventListener("click", showHint);
+//setting initial condition
+function init() {
+  answer = "";
+  hint = "";
+  life = 10;
+  wordDisplay = [];
+  winningCheck = "";
+  context.clearRect(0, 0, 400, 400);
+  canvas();
+  containerHint.innerHTML = `Clue -`;
+  livesDisplay.innerHTML = `You have ${life} lives!`;
+  setAnswer();
+  container.innerHTML = generateButton();
+  container.addEventListener("click", handleClick);
+  console.log(answer);
+  //console.log(hint);
+}
+
+window.onload = init();
+
+//reset (play again)
+buttonReset.addEventListener("click", init);
+
+//guess click
+function guess(event) {
+  const guessWord = event.target.id;
+  const answerArray = answer.split("");
+  var counter = 0;
+  if (answer === winningCheck) {
+    livesDisplay.innerHTML = `YOU WIN!`;
+    return;
+  } else {
+    if (life > 0) {
+      for (var j = 0; j < answer.length; j++) {
+        if (guessWord === answerArray[j]) {
+          wordDisplay[j] = guessWord;
+          console.log(guessWord);
+          answerDisplay.innerHTML = wordDisplay.join(" ");
+          winningCheck = wordDisplay.join("");
+          //console.log(winningCheck)
+          counter += 1;
+        }
+      }
+      if (counter === 0) {
+        life -= 1;
+        counter = 0;
+        animate();
+      } else {
+        counter = 0;
+      }
+      if (life > 1) {
+        livesDisplay.innerHTML = `You have ${life} lives!`;
+      } else if (life === 1) {
+        livesDisplay.innerHTML = `You have ${life} life!`;
+      } else {
+        livesDisplay.innerHTML = `GAME OVER!`;
+      }
+    } else {
+      return;
+    }
+    console.log(wordDisplay);
+    //console.log(counter);
+    //console.log(life);
+    if (answer === winningCheck) {
+      livesDisplay.innerHTML = `YOU WIN!`;
+      return;
+    }
+  }
+}
+
+container.addEventListener("click", guess);
+
+// Hangman
+function animate() {
+  drawArray[life]();
+  //console.log(drawArray[life]);
+}
+
+function canvas() {
+  myStickman = document.getElementById("stickman");
+  context = myStickman.getContext("2d");
+  context.beginPath();
+  context.strokeStyle = "#fff";
+  context.lineWidth = 2;
+}
+
+function head() {
+  myStickman = document.getElementById("stickman");
+  context = myStickman.getContext("2d");
+  context.beginPath();
+  context.arc(60, 25, 10, 0, Math.PI * 2, true);
+  context.stroke();
+}
+
+function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
+  context.moveTo($pathFromx, $pathFromy);
+  context.lineTo($pathTox, $pathToy);
+  context.stroke();
+}
+
+function frame1() {
+  draw(0, 150, 150, 150);
+}
+
+function frame2() {
+  draw(10, 0, 10, 600);
+}
+
+function frame3() {
+  draw(0, 5, 70, 5);
+}
+
+function frame4() {
+  draw(60, 5, 60, 15);
+}
+
+function torso() {
+  draw(60, 36, 60, 70);
+}
+
+function rightArm() {
+  draw(60, 46, 100, 50);
+}
+
+function leftArm() {
+  draw(60, 46, 20, 50);
+}
+
+function rightLeg() {
+  draw(60, 70, 100, 100);
+}
+
+function leftLeg() {
+  draw(60, 70, 20, 100);
+}
+
+var drawArray = [
+  rightLeg,
+  leftLeg,
+  rightArm,
+  leftArm,
+  torso,
+  head,
+  frame4,
+  frame3,
+  frame2,
+  frame1
+];
